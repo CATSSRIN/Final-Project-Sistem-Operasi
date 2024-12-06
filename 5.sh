34ced4625451e99a8369aec84a2560a72d725a31
CATSS <85265087+CATSSRIN@users.noreply.github.com>
2024-12-06 13:23:14 +0700

    added code

---
+++ 5.sh
@@ -0,0 +1,13 @@
+
+if ! command -v bc &> /dev/null
+then
+    echo "bc could not be found, please install it to proceed."
+    exit
+fi
+
+echo "Masukkan panjang sisi persegi: "
+read sisi
+
+luas=$(echo "$sisi * $sisi" | bc)
+
+echo "Luas persegi dengan panjang sisi $sisi adalah $luas"
